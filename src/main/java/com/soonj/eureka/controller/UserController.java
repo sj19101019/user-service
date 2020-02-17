d87c3ff83c2986c1841bf753b34c78db4c8b9c9f
jeff_hu <jeff@kuma-gaming.com>
2020-02-17 12:12:46 +0800

    build setting

--- src/main/java/com/soonj/eureka/controller/UserController.java
+++ src/main/java/com/soonj/eureka/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "user")
 public class UserController {
 	
-	@Value(value = "foo")
+	@Value(value = "${foo}")
 	private String foo;
 	
 	@GetMapping(value = "greeting")
